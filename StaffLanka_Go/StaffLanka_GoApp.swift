1259919e51ea3a1fb15dabf068a3e4675149b83e
Liviru-Nava <livirunavaratna@gmail.com>
2026-03-23 20:52:53 +0530

    Initialised Project

---
+++ StaffLanka_Go/StaffLanka_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaffLanka_GoApp.swift
+//  StaffLanka_Go
+//
+//  Created by Liviru Navaratna on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct StaffLanka_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
